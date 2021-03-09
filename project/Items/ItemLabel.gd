0a35a83dff1ecdd528599765993518d8e4532ca2
Alex Gestwicki <alex.gestwicki@gmail.com>
2021-03-09 08:21:26 -0500

    Make nicer text appear on the popup labels

--- project/Items/ItemLabel.gd
+++ project/Items/ItemLabel.gd
@@ -7,6 +7,8 @@ extends Label
 
 # constants
 const STYLEBOX := "res://Items/ItemLabel.tres"
+const FONT := "res://Font.tres"
+const COLOR := Color("37ffff")
 
 # exported variables
 
@@ -18,6 +20,8 @@ var _ignore
 
 func _ready()->void:
 	set("custom_styles/normal", load(STYLEBOX))
+	set("custom_fonts/font", load(FONT))
+	set("custom_colors/font_color", COLOR)
 	margin_left = 34
 	margin_top = -16
 	margin_right = 180
@@ -30,6 +34,33 @@ func generate_text(item:Dictionary):
 func _parse_traits_from_item(item:Dictionary)->String:
 	var string := ""
 	string += item["name"]
-	for property in item["properties"]:
-		string += "\n"+property+" "+str(item["properties"][property])
+	string += _get_edited_string(item["properties"])
 	return string
+
+
+func _get_edited_string(properties:Dictionary)->String:
+	var properties_as_string := ""
+	for property in properties:
+		var property_name:String = property
+		var value:float = properties[property]
+		if property_name == "_speed":
+			value /= 10
+		properties_as_string += "\n"
+		property_name = _remove_underscores(property_name)
+		property_name = property_name.capitalize()
+		if value > 0:
+			properties_as_string += "+"+str(value)+" "
+		else:
+			properties_as_string += str(value)+" "
+		properties_as_string += property_name
+	return properties_as_string
+
+
+func _remove_underscores(string:String)->String:
+	var _string_without_underscores := ""
+	for character in string:
+		if character == "_":
+			_string_without_underscores += " "
+		else:
+			_string_without_underscores += character
+	return _string_without_underscores
